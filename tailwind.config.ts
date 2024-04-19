2f8972210ed7e8c4040dede6b85603f007d68df4
JangHoonSeok <orirori512@naver.com>
2024-04-19 23:42:41 +0900

    🔧 chore: src 폴더 생성

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,9 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ".src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    ".src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    ".src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
