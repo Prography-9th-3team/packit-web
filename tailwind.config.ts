68ad31e62e94583f55e926cfd1c1909a68ea8a5e
bbung <tjd123123@naver.com>
2024-04-25 23:33:45 +0900

    🔧 chore: tailwind content path 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,9 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    '.src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    '.src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    '.src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -16,4 +16,5 @@ const config: Config = {
   },
   plugins: [require('tailwindcss-animate')],
 };
+
 export default config;
