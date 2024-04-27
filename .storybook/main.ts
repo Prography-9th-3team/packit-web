18e681ae4a8c440a85f8e06e999922023a919768
bbung <tjd123123@naver.com>
2024-04-27 22:05:00 +0900

    CHORE: 🔧  storybook version downgrade

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,13 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
-    '@storybook/addon-styling-webpack',
   ],
   framework: {
     name: '@storybook/nextjs',
