e8174f7421159af01a142112cc17fa3eb4921844
JangHoonSeok <orirori512@naver.com>
2024-04-30 01:36:49 +0900

    REFACTOR: ♻️  color 팔레트 수정 및 Storybook 맞춰 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <main className=''>
+    <main>
       <h1 className='heading-5xl-bd text-primary'>Main Header</h1>
       <h1 className='heading-4xl-bd text-primary-hover'>Main Header</h1>
       <h1 className='heading-3xl-bd text-primary-pressed'>Main Header</h1>
@@ -10,11 +10,13 @@ const Home = () => {
       <h1 className='heading-md text-critical'>Main Header</h1>
       <h1 className='body-lg text-critical-hover'>Main body</h1>
       <h1 className='body-md text-critical-pressed'>Main body</h1>
+      <h1 className='body-md text-action'>Main body</h1>
       <h1 className='body-sm'>Main body</h1>
       <h1 className='label-3xl-bold'>Main label</h1>
       <h1 className='label-xl-bold'>Main label</h1>
       <h1 className='label-lg'>Main label</h1>
       <h1 className='label-md-bold'>Main label</h1>
+
       <div className='flex gap-16'>
         <button className='p-40 shadow-base'>button</button>
         <button className='p-40 shadow-md'>button</button>
--- styles/theme/colors.ts
+++ styles/theme/colors.ts
@@ -1,110 +1,115 @@
 export const colors = {
-  // primary
-  primary: {
-    DEFAULT: '#7BC600',
-    hover: '#A3D74D',
-    pressed: '#BDE280',
+  action: {
+    primary: {
+      default: '#7BC600',
+      hover: '#A3D74D',
+      pressed: '#BDE280',
+      tonal: '#7BC6001A',
+      disabled: '#EAEBEC',
+    },
+    secondary: {
+      hover: '#F8F9FB',
+      pressed: '#E2E5E9',
+      disabled: '#EAEBEC',
+    },
+    critical: {
+      default: '#FE5856',
+      hover: '#FE5868',
+      pressed: '#FECDD3',
+      disabled: '#EAEBEC',
+    },
   },
 
-  // action primary
-  'action-primary': {
-    DEFAULT: '#7BC600',
-    hover: '#A3D74D',
-    pressed: '#BDE280',
-    tonal: '#7BC6001A',
-    disabled: '#EAEBEC',
-  },
-
-  // secondary
-  secondary: {
-    DEFAULT: '#717680',
-    hover: '#9FA3AB',
-    pressed: '#E2E5E9',
-  },
-
-  // action- secondary
-  'action-secondary': {
-    hover: '#F8F9FB',
-    pressed: '#E2E5E9',
-    disabled: '#EAEBEC',
-  },
-
-  // critical
-  critical: {
-    DEFAULT: '#FE5868',
-    hover: '#FE7683',
-    pressed: '#FECDD3',
-  },
-
-  // action critical
-  'action-critical': {
-    DEFAULT: '#FE5856',
-    hover: '#FE5868',
-    pressed: '#FECDD3',
-    disabled: '#EAEBEC',
-  },
-
-  // border
   border: {
-    DEFAULT: '#DBDEE3',
+    default: '#DBDEE3',
     'critical-hover': '#FEA3AC',
-    focuse: '#7BC600',
+    focus: '#7BC600',
     hover: '#A3D74D',
   },
 
-  // divide
   divide: {
-    DEFAULT: '#E2E5E9',
+    default: '#E2E5E9',
     primary: '#7BC600',
     minimal: '#EFF1F4',
     on: '#15181E',
   },
 
-  // icon
+  'filed-disabled': {
+    default: '#7378820D',
+  },
+  backdrop: {
+    default: 'rgba(0, 0, 0, 0.15)',
+  },
+
   icon: {
-    DEFAULT: '#4B515B',
-    primary: '#7BC600',
-    secondary: '#717680',
-    critical: '#FE5868',
+    default: '#4B515B',
     disabled: '#BCC0C6',
     minimal: '#9298A7',
     sub: '#717680',
     on: '#FFFFFF',
-    'primary-hover': '#A3D74D',
-    'critical-hover': '#FE7683',
-    'secondary-hover': '#9FA3AB',
+    critical: {
+      default: '#FE5868',
+      hover: '#FE7683',
+    },
+    primary: {
+      default: '#7BC600',
+      hover: '#A3D74D',
+    },
+    secondary: {
+      default: '#717680',
+      hover: '#9FA3AB',
+    },
   },
 
-  // surface
   surface: {
-    DEFAULT: '#FFFFFF',
+    default: '#FFFFFF',
     critical: '#FFF2F3',
     empty: '#E2E5E9',
     minimal: '#FDFDFD',
     sub: '#F8F9FB',
   },
 
-  // text
   text: {
-    DEFAULT: '#15181E',
-    primary: '#7BC600',
-    'primary-hover': '#A3D74D',
-    secondary: '#717680',
-    'secondary-hover': '#9FA3AB',
-    critical: '#FE7683',
+    default: '#15181E',
     disabled: '#BCC0C6',
     minimal: '#9298A7',
     on: '#FFFFFF',
     sub: '#717680',
+
+    critical: {
+      default: '#FE5868',
+      hover: '#FE7683',
+    },
+    primary: {
+      default: '#7BC600',
+      hover: '#A3D74D',
+    },
+    secondary: {
+      default: '#717680',
+      hover: '#9FA3AB',
+    },
   },
 
-  'filed-disabled': {
-    DEFALUT: '#7378820D',
+  // 아래부터는 팔레트
+  primary: {
+    default: '#7BC600',
+    hover: '#A3D74D',
+    pressed: '#BDE280',
   },
-  backdrop: {
-    DEFAULT: '#00000026',
+
+  secondary: {
+    default: '#717680',
+    hover: '#9FA3AB',
+    pressed: '#E2E5E9',
   },
+
+  critical: {
+    default: '#FE5868',
+    hover: '#FE7683',
+    pressed: '#FECDD3',
+  },
+
   disabled: {
-    DEFAULT: '#EAEBEC',
+    default: '#EAEBEC',
   },
 };
