a62d7cc9f9e79ad60e17ed628b73614763d88027
JangHoonSeok <orirori512@naver.com>
2024-05-01 03:36:36 +0900

    FEAT: ✨  svg 처리를 위한 custom.d.ts 추가

---
+++ src/components/common/Icon/custom.d.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const content: any;
+  export default content;
+}
