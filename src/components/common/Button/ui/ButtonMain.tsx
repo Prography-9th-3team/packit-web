a735d7be5bc2c4434235c360b699c88d0aa5480d
JangHoonSeok <orirori512@naver.com>
2024-04-30 17:38:35 +0900

    DESIGN: 💄  button 기본 css variants 설정

--- src/components/common/Button/ui/ButtonMain.tsx
+++ src/components/common/Button/ui/ButtonMain.tsx
@@ -1,9 +1,7 @@
+import { VariantProps, cva } from 'class-variance-authority';
 import { PropsWithChildren } from 'react';
-import { BUTTON_SIZE, BUTTON_TYPE } from '../types';
 
-interface IButtonMain extends PropsWithChildren {
-  type: BUTTON_TYPE[keyof BUTTON_TYPE];
-  size: BUTTON_SIZE[keyof BUTTON_SIZE];
+interface IButtonMain extends PropsWithChildren, VariantProps<typeof buttonVariants> {
   isLoading?: boolean;
   isIconMoe?: boolean;
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
@@ -12,5 +10,23 @@ interface IButtonMain extends PropsWithChildren {
 const ButtonMain = ({ children }: IButtonMain) => {
   return <div>{children}</div>;
 };
+
+const buttonVariants = cva(['flex justify-center items-center'], {
+  variants: {
+    size: {
+      large: 'min-w-80 px-16 py-12 rounded-lg gap-8',
+      medium: 'min-w-64 px-12 py-8 rounded-lg gap-8',
+      small: 'px-12 py-4 rounded-md gap-4',
+      tiny: 'px-8 py-6 rounded-md gap-4',
+    },
+    type: {
+      primary: 'bg-action-primary hover:bg-action-primary-hover active:bg-action-primary-pressed',
+      outline:
+        'bg-surface border-[1px] border-solid border-border hover:bg-action-secondary-hover active:bg-action-secondary-pressed',
+      secondary: 'hover:bg-action-secondary-hover active:bg-action-secondary-pressed',
+      critical: 'bg-critical hover:bg-critical-hover active:bg-critical-pressed',
+    },
+  },
+});
 
 export default ButtonMain;
