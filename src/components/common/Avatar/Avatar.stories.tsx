023cb693730e8043381cf7b1c9932e08fd0b9a86
JangHoonSeok <orirori512@naver.com>
2024-05-01 18:14:47 +0900

    TEST: ✅  photo url 있는 avatar story 생성

--- src/components/common/Avatar/Avatar.stories.tsx
+++ src/components/common/Avatar/Avatar.stories.tsx
@@ -24,3 +24,10 @@ export const Primary: Story = {
     size: AVATAR_SIZE.LG,
   },
 };
+
+export const PhotoAvatar: Story = {
+  args: {
+    profileUrl: 'https://avatars.githubusercontent.com/u/56246060?v=4',
+    size: AVATAR_SIZE.LG,
+  },
+};
