f9221c4f905faeb15e310eeecbd13d08ba6d997e
JangHoonSeok <orirori512@naver.com>
2024-05-10 13:00:27 +0900

    FEAT: ✨  Menu 컴포넌트 isSelected state 추가

--- src/components/common/Menu/ui/MenuMain.tsx
+++ src/components/common/Menu/ui/MenuMain.tsx
@@ -1,22 +1,35 @@
 import { cn } from '@/lib/utils';
+import { cva } from 'class-variance-authority';
 import { PropsWithChildren } from 'react';
+import { MenuContext } from '../module/MenuStateContext';
 
 export interface IMenuMain extends PropsWithChildren {
   onClick?: () => void;
+  isSelected?: boolean;
 }
 
-const MenuMain = ({ children, onClick }: IMenuMain) => {
+const MenuMain = ({ children, onClick, isSelected = false }: IMenuMain) => {
   return (
-    <button
-      className={cn([
-        'flex w-full px-12 py-6 items-center gap-12 rounded-md text-text text-ellipsis',
-        'hover:bg-surface-sub',
-      ])}
-      onClick={onClick}
-    >
-      {children}
-    </button>
+    <MenuContext.Provider value={{ isSelected }}>
+      <button className={cn(menuVariants({ isSelected }))} onClick={onClick}>
+        {children}
+      </button>
+    </MenuContext.Provider>
   );
 };
+
+export const menuVariants = cva(
+  [
+    'flex w-full px-12 py-6 items-center gap-12 rounded-md text-text text-ellipsis',
+    'hover:bg-surface-sub',
+  ],
+  {
+    variants: {
+      isSelected: {
+        true: 'bg-action-primary-tonal text-text-primary hover:bg-action-primary-tonal',
+      },
+    },
+  },
+);
 
 export default MenuMain;
