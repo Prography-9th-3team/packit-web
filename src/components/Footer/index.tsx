f9351a1ce77ee6b7c7c52e6e02bfba9b3e0eed80
bbung <tjd123123@naver.com>
2024-06-03 01:36:46 +0900

    FEAT: ✨  footer 개인정보처리방침 추가

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,4 @@
+import Divider from '../common/Divider';
 import Logo from '../common/Logo';
 
 /**
@@ -9,7 +10,11 @@ const Footer = () => {
   return (
     <footer className='w-full h-[148px] py-24 flex flex-col justify-center items-center gap-10 bg-surface'>
       <Logo width={66.56} height={16} className='text-[#15181E] opacity-50' />
-      <div className='text-text-minimal label-md'>copyright © packit. All rights reserved</div>
+      <div className='flex items-center gap-8 text-text-minimal label-md'>
+        <a href='#'>개인정보처리방침</a>
+        <Divider direction='vertical' className='h-1/3 bg-divide-minimal' />
+        <div>copyright © packit. All rights reserved</div>
+      </div>
     </footer>
   );
 };
