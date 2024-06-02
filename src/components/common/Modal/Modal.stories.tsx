181431fdd2489e620322fcd496bd92bc97363cff
bbung <tjd123123@naver.com>
2024-06-02 17:37:16 +0900

    TEST: ✅  북마크 모달 스토리북 수정

--- src/components/common/Modal/Modal.stories.tsx
+++ src/components/common/Modal/Modal.stories.tsx
@@ -2,7 +2,7 @@ import useModalStore from '@/stores/modalStore';
 import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from '../Button';
 import Modal from './ModalLayout';
-import ModalPortal from './ModalPortal';
+import BookmarkModal from './ui/BookmarkModal';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
@@ -17,22 +17,20 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Default: Story = {
+export const Bookmark: Story = {
   args: {},
   render: () => {
-    const { openModal } = useModalStore();
+    const { openModal, isModalOpen } = useModalStore();
 
     return (
       <>
-        <Button type='outline' onClick={openModal}>
+        <Button type='outline' onClick={() => openModal('bookmarkModal')}>
           <Button.Label>Open</Button.Label>
         </Button>
 
         <div id='modal'></div>
 
-        <ModalPortal>
-          <div>Modal Open</div>
-        </ModalPortal>
+        {isModalOpen('bookmarkModal') && <BookmarkModal />}
       </>
     );
   },
