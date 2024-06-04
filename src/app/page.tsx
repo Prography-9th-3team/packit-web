906c095624cb7313fc73b77bfd755a5af4bdd9fa
bbung <tjd123123@naver.com>
2024-06-04 19:04:12 +0900

    STYLE: 🎨  header 영역 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 'use client';
 
 const Home = () => {
-  return <main className='bg-[#D9D9D9]'></main>;
+  return (
+    <main className='bg-surface'>
+      <header className='p-40'>
+        <h1 className='text-text heading-3xl-bd'>내 북마크</h1>
+      </header>
+    </main>
+  );
 };
 
 export default Home;
