d9dc33209f8992f95a9a37b41b9d6eeb655ee9ad
bbung <tjd123123@naver.com>
2024-06-15 15:19:22 +0900

    FEAT: ✨  라우터 가드 추가 (임시)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+'use client';
+
 import ContentBox from '@/components/ContentBox';
 import FilterBox from '@/components/FilterBox';
-import { Suspense } from 'react';
+import { useRouter } from 'next/navigation';
+import { Suspense, useEffect } from 'react';
 
 const Home = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!document.cookie.includes('accessToken')) {
+      router.push('/login');
+    }
+  }, []);
+
   return (
     <main className='bg-surface'>
       <header className='p-40'>
