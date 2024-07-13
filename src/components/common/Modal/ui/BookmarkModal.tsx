9bb0794739f856e4b8eb2423124a4533a3bc108c
JangHoonSeok <orirori512@naver.com>
2024-07-13 15:09:09 +0900

    FEAT: ✨  overflow-scroll일 때 hide-scroll 적용해서 북마크 추가 모달 style 수정

--- src/components/common/Modal/ui/BookmarkModal.tsx
+++ src/components/common/Modal/ui/BookmarkModal.tsx
@@ -110,7 +110,7 @@ const BookmarkModal = () => {
 
   return (
     <ModalPortal>
-      <div className='w-[432px]'>
+      <div className='w-[432px] overflow-hidden'>
         <div className='mb-24'>
           <h1 className='text-text heading-2xl-bd'>북마크 추가</h1>
         </div>
