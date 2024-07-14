cbc3e7a20afcb18701ea9b6bcd5f74613694d0e9
JangHoonSeok <orirori512@naver.com>
2024-07-14 23:11:44 +0900

    FEAT: ✨  카테고리 리스트 쪽 css 적용완료

--- src/components/TabList/index.tsx
+++ src/components/TabList/index.tsx
@@ -28,47 +28,57 @@ const TabList = ({ tabs = [] }: ITabList) => {
   const queryTab = queryParam.get('tab') ?? '전체';
 
   return (
-    <ul
-      className={cn(
-        'h-40 flex gap-28 whitespace-nowrap hide-scroll relative select-none overflow-x-scroll',
-        isControlModalOpen && 'overflow-visible',
-      )}
-    >
-      {tabs.map((tab, index) => (
-        <li
-          key={tab.categoryId}
-          className={cn([
-            'cursor-pointer flex gap-2 label-md-bold text-text transition-all duration-150 items-center relative pb-16',
-            queryTab === String(tab.categoryId ?? '전체') && 'border-b-2 border-divide-on',
-          ])}
-          onClick={() => handleClickCategory(tab.categoryId)}
-          onMouseEnter={() => setIsHover(index)}
-          onMouseLeave={() => setIsHover(null)}
-        >
-          <div className='flex gap-4'>
-            {tab.categoryName} <span className='text-primary'>{tab.bookMarkCount}</span>
-          </div>
-          {(isHover === index ||
-            (isControlModalOpen && queryTab === String(tab.categoryId ?? '전체'))) && (
-            <Icon
-              name='dotsVertical'
-              className='w-16 h-16 text-text-minimal absolute left-[calc(100%-2px)]'
-              onClick={handleModal}
-            />
-          )}
-          {isControlModalOpen && queryTab === String(tab.categoryId ?? '전체') && (
-            <div className='absolute top-[calc(100%-8px)] left-[calc(100%-10px)] w-[165px] h-[100px] flex flex-col gap-4 rounded-lg shadow-layer bg-white overflow-hidden z-10'>
-              <Option>
-                <Option.Label>이름 수정</Option.Label>
-              </Option>
-              <Option>
-                <Option.Label>삭제</Option.Label>
-              </Option>
+    <div className='h-40 relative w-[calc(100%-100px)]'>
+      <ul
+        className={cn(
+          'absolute w-full h-[500px] flex gap-28 whitespace-nowrap hide-scroll select-none overflow-x-scroll',
+        )}
+      >
+        {tabs.map((tab, index) => (
+          <li
+            key={tab.categoryId}
+            className={cn([
+              'cursor-pointer flex h-[40px] gap-2 label-md-bold text-text transition-all duration-150 items-center relative pb-16',
+              queryTab === String(tab.categoryId ?? '전체') && 'border-b-2 border-divide-on',
+            ])}
+            onClick={() => handleClickCategory(tab.categoryId)}
+            onMouseEnter={() => setIsHover(index)}
+            onMouseLeave={() => setIsHover(null)}
+          >
+            <div className='flex gap-4'>
+              {tab.categoryName} <span className='text-primary'>{tab.bookMarkCount}</span>
             </div>
-          )}
-        </li>
-      ))}
-    </ul>
+            {(isHover === index ||
+              (isControlModalOpen && queryTab === String(tab.categoryId ?? '전체'))) && (
+              <Icon
+                name='dotsVertical'
+                className='w-16 h-16 text-text-minimal absolute left-[calc(100%-2px)]'
+                onClick={handleModal}
+              />
+            )}
+            {isControlModalOpen && queryTab === String(tab.categoryId ?? '전체') && (
+              <div className='absolute top-[calc(100%-8px)] shadow-layer left-[calc(100%-10px)] w-[165px] h-[100px] flex flex-col gap-4 rounded-lg bg-white overflow-hidden z-10'>
+                <Option>
+                  <Option.Label>이름 수정</Option.Label>
+                </Option>
+                <Option>
+                  <Option.Label>
+                    <span className='text-text-critical'>삭제</span>
+                  </Option.Label>
+                </Option>
+              </div>
+            )}
+          </li>
+        ))}
+      </ul>
+      <div
+        className='w-20 h-24 absolute right-0'
+        style={{
+          background:
+            'linear-gradient(270deg, #FFF 0%, rgba(255, 255, 255, 0.95) 25%, rgba(255, 255, 255, 0.85) 41.02%, rgba(255, 255, 255, 0.78) 50.13%, rgba(255, 255, 255, 0.65) 61.75%, rgba(255, 255, 255, 0.45) 73.87%, rgba(255, 255, 255, 0.25) 86.25%, rgba(255, 255, 255, 0.00) 100%)',
+        }}
+      />
+    </div>
   );
 };
 
