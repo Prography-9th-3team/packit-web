fd4b1dcc6496ef55240d4ba10d03f10a918d545b
JangHoonSeok <orirori512@naver.com>
2024-07-26 08:47:55 +0900

    FEAT: ✨  feed 컴포넌트 생성

---
+++ src/components/Feed/index.tsx
@@ -0,0 +1,9 @@
+const Feed = () => {
+  return (
+    <div className='h-full fixed right-0 flex px-12 items-center gap-[11px] flex-col bg-surface'>
+      index
+    </div>
+  );
+};
+
+export default Feed;
