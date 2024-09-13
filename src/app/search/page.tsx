17787910a938797e999457ef79dee3796b414d10
bbung <tjd123123@naver.com>
2024-09-13 17:27:59 +0900

    CHORE: 🔧  search page suspense 수정

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import FilterBox from '@/components/FilterBox';
 import SearchContentBox from '@/components/SearchContentBox';
 import { Suspense } from 'react';
@@ -8,8 +6,8 @@ import SearchHeader from './components/SearchHeader';
 const Search = () => {
   return (
     <main className='min-w-[755px] bg-surface'>
-      <SearchHeader />
       <Suspense>
+        <SearchHeader />
         <FilterBox />
         {/* 컨텐츠 영역 */}
         <SearchContentBox />
