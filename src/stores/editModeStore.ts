2e29dbda783030e8fafc3f39bb65c39966b9c5b0
JangHoonSeok <orirori512@naver.com>
2024-09-21 18:41:38 +0900

    FEAT: ✨  edit mode store에 editmode 제어 추가

--- src/stores/editModeStore.ts
+++ src/stores/editModeStore.ts
@@ -1,23 +1,42 @@
 import { create } from 'zustand';
 
 type Store = {
+  isEditMode: boolean;
+  setEditMode: (isEditMode: boolean) => void;
   selectedBookmarks: Array<{
     bookmarkId: number;
     url: string;
   }>;
+  isSelectedBookmark: (bookmarkId: number) => boolean;
   setSelectedBookmarks: ({ bookmarkId, url }: { bookmarkId: number; url: string }) => void;
   resetSelectedBookmarks: () => void;
   getSelectedBookmarksLength: () => number;
 };
 
 const useEditModeStore = create<Store>((set, get) => ({
+  isEditMode: false,
   selectedBookmarks: [],
+
+  setEditMode: (isEditMode: boolean) => {
+    set(() => ({
+      isEditMode,
+    }));
+  },
 
   // @desc: 선택된 북마크 추가
   setSelectedBookmarks: ({ bookmarkId, url }) => {
+    const isAlreadyExist = get().selectedBookmarks.some((item) => item.bookmarkId === bookmarkId);
+
     set((state) => ({
-      selectedBookmarks: [...state.selectedBookmarks, { bookmarkId, url }],
+      // @desc: toggle 되게끔
+      selectedBookmarks: isAlreadyExist
+        ? state.selectedBookmarks.filter((item) => item.bookmarkId !== bookmarkId)
+        : [...state.selectedBookmarks, { bookmarkId, url }],
     }));
+  },
+
+  isSelectedBookmark: (bookmarkId: number) => {
+    return get().selectedBookmarks.some((item) => item.bookmarkId === bookmarkId);
   },
 
   // @desc: 선택된 북마크 개수 반환
